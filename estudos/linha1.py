9430674236bd49fb05227c50e20bbc93bc8e671e
Vinicius Almeida <vinysilvaa20@gmail.com>
2025-08-14 13:41:54 -0300

    a

---
+++ estudos/linha1.py
@@ -0,0 +1,23 @@
+# print('hello world')
+
+# int  = 1,2,3,4,5,6
+# float = 1,4 . 2,6 , 1.2.3.4.5.6.
+# str = 'carlos'ca
+# bool = True // False
+
+
+
+
+
+alvaro = float(input('Dinheiro: '))
+print(alvaro)
+
+
+
+# Variáveis e boas práticas de nomeação.
+
+# Operadores: aritméticos, comparação, lógicos e de atribuição.
+
+# Função input() para receber dados do usuário.
+
+# Conversão de tipos (int(), float(), str()).
